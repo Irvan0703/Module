aa01a177aa57699182926b1b05fb8ab1976593b8
Irvan0703 <muhamadirfanulhadi@gmail.com>
2022-03-14 13:43:36 +0700

    Tugas Module Grid

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import hello from './module';
+import {Grid} from './module.js';
 
-hello();
+
+let tabel = new Grid({
+    columns:['Name', 'Email', 'Phone Number'],
+    data: [
+      ['John', 'john@example.com', '(353) 01 222 3333'],
+      ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
+    ]
+  });
+tabel.render(document.getElementById("tab"));
 
 
--- module.js
+++ module.js
@@ -1,3 +1,37 @@
-const hello = () => console.log("hello");
+class Grid{
+    constructor(input){
+        this.input=input;
+    }
+    generateHeader(columns){
+        let open = "<head><tr>";
+        let close = "</tr></head>";
+        columns.forEach((d) => {
+          open += `<th>${d}</th>`;
+        });
+  
+        return open + close;
+      }
+      generateData(data){
+        let open = "<body>";
+        let close = "<body>";
+  
+        data.forEach((d) => {
+         open += `
+            <tr>
+              <td>${d[0]}</td>
+              <td>${d[1]}</td>
+              <td>${d[2]}</td>
+              </tr>
+           `;
+          });
+        return open + close;
+      }
+      render(element){
+      let data = this.generateData(this.input.data);
+      let column = this.generateHeader(this.input.columns);
+      let isi = "<table class='table table-hover'>"+column+data+"</table>";
+       element.innerHTML = isi;
+      }
+}
 
-export default hello;
+export {Grid};
